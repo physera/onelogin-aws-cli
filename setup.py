25b659ecec6164b0141b52a6faf12b24a6ddd767
slycoder <slycoder@gmail.com>
2018-01-13 13:44:38 -0800

    Set a minimum python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name='onelogin_aws_cli',
     packages=['onelogin_aws_cli'],
     version='0.1.7',
-
+    python_requires='>=3',
     description='Onelogin assume AWS role through CLI',
     classifiers=[
         'Development Status :: 3 - Alpha',
