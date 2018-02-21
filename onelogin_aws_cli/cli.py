7cde2970828e7896ed53b0760c1fe06d23f03f86
Drew J. Sonne <drew.sonne@gmail.com>
2018-02-21 08:08:56 +0000

    Modified login entrypoint to be testable

--- onelogin_aws_cli/cli.py
+++ onelogin_aws_cli/cli.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 import sys
 import time
@@ -8,36 +6,43 @@ from onelogin_aws_cli import OneloginAWS, \
     DEFAULT_CONFIG_PATH
 from onelogin_aws_cli.configuration import ConfigurationFile
 
-parser = argparse.ArgumentParser(description="Login to AWS with Onelogin")
-parser.add_argument("-c", "--configure", dest="configure", action="store_true",
-                    help="Configure Onelogin and AWS settings")
-parser.add_argument("-C", "--config_name", default="default",
-                    help="Switch configuration name within config file")
-parser.add_argument("--profile", default="",
-                    help="Specify profile name of credential")
-parser.add_argument("-u", "--username", default="",
-                    help="Specify OneLogin username")
-parser.add_argument("-r", "--renewSeconds", type=int,
-                    help="Auto-renew credentials after this many seconds")
 
-args = parser.parse_args()
+def login(args=sys.argv[1:]):
+    """
+    Entrypoint for `onelogin-aws-login`
+    :param args:
+    """
+    parser = argparse.ArgumentParser(description="Login to AWS with Onelogin")
+    parser.add_argument("-c", "--configure", dest="configure",
+                        action="store_true",
+                        help="Configure Onelogin and AWS settings")
+    parser.add_argument("-C", "--config_name", default="default",
+                        help="Switch configuration name within config file")
+    parser.add_argument("--profile", default="",
+                        help="Specify profile name of credential")
+    parser.add_argument("-u", "--username", default="",
+                        help="Specify OneLogin username")
+    parser.add_argument("-r", "--renewSeconds", type=int,
+                        help="Auto-renew credentials after this many seconds")
 
-with open(DEFAULT_CONFIG_PATH) as fp:
-    configFile = ConfigurationFile(fp)
+    args = parser.parse_args(args)
 
-if args.configure:
-    configFile.initialise()
+    with open(DEFAULT_CONFIG_PATH) as fp:
+        configFile = ConfigurationFile(fp)
 
-config_section = configFile.section(args.config_name)
+    if args.configure:
+        configFile.initialise()
 
-if config_section is None:
-    sys.exit("Configuration '{}' not defined. "
-             "Please run 'onelogin-aws-login -c'".format(args.config_name))
+    config_section = configFile.section(args.config_name)
 
-api = OneloginAWS(config_section, args)
-api.save_credentials()
+    if config_section is None:
+        sys.exit("Configuration '{}' not defined. "
+                 "Please run 'onelogin-aws-login -c'".format(args.config_name))
 
-if args.renewSeconds:
-    while True:
-        time.sleep(args.renewSeconds)
-        api.save_credentials()
+    api = OneloginAWS(config_section, args)
+    api.save_credentials()
+
+    if args.renewSeconds:
+        while True:
+            time.sleep(args.renewSeconds)
+            api.save_credentials()
