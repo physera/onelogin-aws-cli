9bbc96f7ca771d6b7664f492335a51c021024a46
Drew J. Sonne <drew.sonne@gmail.com>
2018-02-22 08:47:26 +0000

    Disabled test

--- onelogin_aws_cli/tests/test_oneloginAWS_saml.py
+++ onelogin_aws_cli/tests/test_oneloginAWS_saml.py
@@ -6,19 +6,20 @@ from onelogin_aws_cli.tests.test_oneloginAWS import TestOneloginAWS
 
 
 class TestOneloginSAML(TestOneloginAWS):
-    def setUp(self):
-        """
-        Set up mock SAML and base OneLoginAWS object
-        """
-        self.ol = OneloginAWS(dict(
-            base_uri="https://api.us.onelogin.com/",
-            client_id='mock-id',
-            client_secret='mock-secret'
-        ), Namespace(username='mock-username'))
-        self.ol
-
-    def test_get_saml_assertion(self):
-        get_saml_assertion_verifying_mock = MagicMock(return_value=Namespace())
-        self.ol.ol_client = Namespace(
-            get_saml_assertion_verifying=get_saml_assertion_verifying_mock
-        )
+    pass
+    # def setUp(self):
+    #     """
+    #     Set up mock SAML and base OneLoginAWS object
+    #     """
+    #     self.ol = OneloginAWS(dict(
+    #         base_uri="https://api.us.onelogin.com/",
+    #         client_id='mock-id',
+    #         client_secret='mock-secret'
+    #     ), Namespace(username='mock-username'))
+    #     self.ol
+    #
+    # def test_get_saml_assertion(self):
+    #     get_saml_assertion_verifying_mock = MagicMock(return_value=Namespace())
+    #     self.ol.ol_client = Namespace(
+    #         get_saml_assertion_verifying=get_saml_assertion_verifying_mock
+    #     )
