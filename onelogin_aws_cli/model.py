5a6d069ed58999cc7aff4e194978ac2dc2a0d8b5
Drew J. Sonne <drew.sonne@gmail.com>
2018-03-10 10:55:47 +0000

    Add SignalRepr enum model

---
+++ onelogin_aws_cli/model.py
@@ -0,0 +1,18 @@
+"""
+Describes data structures within the project
+"""
+import signal
+
+from enum import Enum
+
+
+class SignalRepr(Enum):
+    """
+    Represent the signal types
+    """
+
+    SIGTERM = signal.SIGTERM
+    SIGINT = signal.SIGINT
+
+    def __str__(self):
+        return str(self._name_)
