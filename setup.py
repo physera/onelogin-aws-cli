487d981f780bf15a94ba69bcdfe22c0f84001654
Drew J. Sonne <drew.sonne@gmail.com>
2018-03-17 10:29:22 +0000

    Updated PyPi categories
    This feels like we’re a bit further along than alpha now, and we do support python 3 *only*.

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ setuptools.setup(
     python_requires='>=3',
     description='Onelogin assume AWS role through CLI',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only'
         'Programming Language :: Python :: 3.6',
-        'Topic :: Security'
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory'
     ],
     keywords='onelogin aws cli',
     author='Cameron Marlow',
