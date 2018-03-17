13aad7244e5ffffe7831d8b2b960637aebb276fd
Drew J. Sonne <drew.sonne@gmail.com>
2018-03-17 11:53:08 +0000

    Add test cases

--- onelogin_aws_cli/tests/test_oneLoginAWSArgumentParser.py
+++ onelogin_aws_cli/tests/test_oneLoginAWSArgumentParser.py
@@ -1,16 +1,57 @@
+import contextlib
 from unittest import TestCase
 
+from io import StringIO
+
+from onelogin_aws_cli import VERSION
 from onelogin_aws_cli.argparse import OneLoginAWSArgumentParser
 
 
 class TestOneLoginAWSArgumentParser(TestCase):
+
     def test_basic(self):
         parser = OneLoginAWSArgumentParser()
         args = parser.parse_args([
-            '--version'
+            '-C', 'my_config',
+            '--profile', 'my_profile',
+            '-u', 'my_username'
         ])
 
-        self.assertEqual(args, [])
+        self.assertEqual(args.config_name, 'my_config')
+        self.assertEqual(args.profile, 'my_profile')
+        self.assertEqual(args.username, 'my_username')
+
+    def test_version(self):
+        parser = OneLoginAWSArgumentParser()
+        parser.add_cli_options()
+        mock_stdout = StringIO()
+
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(mock_stdout):
+                parser.parse_args([
+                    '--version'
+                ])
+
+        # This spits out the nosetest prog name.
+        # I'm ok with that, as what is important is that the version is
+        # correct
+        self.assertEqual("""_jb_nosetest_runner.py {ver}
+""".format(ver=VERSION), mock_stdout.getvalue())
+
+        self.assertEqual(cm.exception.code, 0)
 
     def test_add_cli_options(self):
-        self.fail()
+        parser = OneLoginAWSArgumentParser()
+        parser.add_cli_options()
+        args = parser.parse_args([
+            '-C', 'my_config',
+            '--profile', 'my_profile',
+            '-u', 'my_username',
+            '--renew-seconds', '30',
+            '-c'
+        ])
+
+        self.assertEqual(args.config_name, 'my_config')
+        self.assertEqual(args.profile, 'my_profile')
+        self.assertEqual(args.username, 'my_username')
+        self.assertTrue(args.configure)
