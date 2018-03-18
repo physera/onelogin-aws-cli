40d51f9b762a0c3eddb15c85c1b75670db770d24
Drew J. Sonne <drew.sonne@gmail.com>
2018-03-18 09:08:17 +0000

    Added base methods for cred source

--- onelogin_aws_cli/credsource/__init__.py
+++ onelogin_aws_cli/credsource/__init__.py
@@ -23,5 +23,70 @@ class CredentialsSource(object):
 
         return cred_type in self._cred_types
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def username(self):
+        """
+        Should be overriden. Retrieves a OneLogin username
+        """
+        raise MissingUsernameException()
+
+    def password(self):
+        """
+        Should be overriden. Retrieves a OneLogin Password
+        :return:
+        """
+
+        raise MissingPasswordException()
+
+    def mfa_device(self):
+        """
+        Should be overriden. Retrieves a OneLogin MFA Device
+        :return:
+        """
+
+        raise MissingMfaDeviceException()
+
+    def mfa_otp(self):
+        """
+        Should be overriden. Retrieves a OneLogin MFA OTP
+        :return:
+        """
+
+        raise MissingMfaOtpException()
+
+
+class MissingCredentialException(Exception):
+    """
+    Raised when a credential is missing.
+    """
+    TYPE = "error"
+
+    def __init__(self):
+        super().__init__("ONELOGIN_" + self.TYPE + "_MISSING")
+
+
+class MissingPasswordException(MissingCredentialException):
+    """
+    Throw when a required password can not be found
+    """
+    TYPE = "PASSWORD"
+
+
+class MissingUsernameException(MissingCredentialException):
+    """
+    Throw when a required password can not be found
+    """
+    TYPE = "USERNAME"
+
+
+class MissingMfaDeviceException(MissingCredentialException):
+    """
+    Throw when a required password can not be found
+    """
+    TYPE = "MFA_DEVICE"
+
+
+class MissingMfaOtpException(MissingCredentialException):
+    """
+    Throw when a required password can not be found
+    """
+    TYPE = "MFA_OTP"
