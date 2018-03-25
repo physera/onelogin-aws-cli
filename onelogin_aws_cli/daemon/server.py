e8ab0fbd555c487418165f0976b048214ef4aaa1
Drew J. Sonne <drew.sonne@gmail.com>
2018-03-25 09:28:35 +0100

    Add documentation

--- onelogin_aws_cli/daemon/server.py
+++ onelogin_aws_cli/daemon/server.py
@@ -1,24 +1,35 @@
 import socketserver
 from threading import Thread
 
+from onelogin_aws_cli.configuration import Section
+
 
 class Server(Thread):
+    """Start server in thread"""
+    HOST = "localhost"
+    PORT = 9999
 
-    def __init__(self, config):
+    def __init__(self, config: Section):
+        super().__init__()
         self.config = config
 
     def run(self):
-        HOST, PORT = "localhost", 9999
+        """Start our server on specificed port"""
 
         # Create the server, binding to localhost on port 9999
-        with socketserver.TCPServer((HOST, PORT), Server) as server:
+        with socketserver.TCPServer((self.HOST, self.PORT),
+                                    ServerHandler) as server:
             # Activate the server; this will keep running until you
             # interrupt the program with Ctrl-C
             server.serve_forever()
 
 
 class ServerHandler(socketserver.BaseRequestHandler):
+    """Handle server requests. Currently performs an echo."""
+
     def handle(self):
+        """Write the data we receive to stdout"""
+
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
