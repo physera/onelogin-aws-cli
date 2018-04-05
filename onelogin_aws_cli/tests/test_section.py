3c1df9387403b9f3d59adcaf34b9e814f4857b3f
Drew J. Sonne <drew.sonne@gmail.com>
2018-04-05 08:44:51 +0100

    Expand test coverage a bit

--- onelogin_aws_cli/tests/test_section.py
+++ onelogin_aws_cli/tests/test_section.py
@@ -6,6 +6,19 @@ from onelogin_aws_cli import Section
 
 
 class TestSection(TestCase):
+
+    def test___contains__true(self):
+        sec = Section('mock-section', Namespace(
+            has_option=MagicMock(return_value=True)
+        ))
+        self.assertTrue('mock' in sec)
+
+    def test___contains__false(self):
+        sec = Section('mock-section', Namespace(
+            has_option=MagicMock(return_value=False)
+        ))
+        self.assertFalse('mock' in sec)
+
     def test_set_overrides(self):
         sec = Section('mock-section', Namespace(
             get=MagicMock(return_value='world')
